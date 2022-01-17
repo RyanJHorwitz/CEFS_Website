700a3dd080a3f47b5cb9e9279ffcba28336c4e79
root <root@DESKTOP-RJ5P570.localdomain>
2022-01-17 10:50:14 -0800

    NAVBAR opens and closes on click rather than hover.  Added JS to accomplish.

---
+++ js/menu-close.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+// Expand menu on click
+$(".dropdown").click(function(){
+	$(this).find(".secLevelMenu").slideToggle("fast");
+		});
+});
+$(document).on("click", function(event){
+	var $trigger = $(".dropdown");
+    if($trigger !== event.target && !$trigger.has(event.target).length){
+        $(".dropdown-menu").slideUp("fast");
+    }            
+});
+
+// Close menu when click outside of it
+$(document).click(function(event) { 
+  var $target = $(event.target);
+  if(!$target.closest('.dropdown').length && 
+  $('.dropdown').is(":visible")) {
+    $('.secLevelMenu').hide();
+  }        
+});
