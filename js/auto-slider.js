fd2ed6ac64dc3037d199d9fdfb56908d60def90a
root <root@DESKTOP-RJ5P570.localdomain>
2022-01-21 14:16:06 -0800

    Functional Dots

--- js/auto-slider.js
+++ js/auto-slider.js
@@ -2,6 +2,7 @@
 
 // Automatic counter
 var counter = 1;
+document.getElementById('radio-1').checked = true;
 setInterval(function(){
 	document.getElementById('radio-' + counter).checked = true;
 	counter++;
@@ -20,6 +21,43 @@ document.getElementById('radio-1').onclick = override;
 document.getElementById('radio-2').onclick = override;
 document.getElementById('radio-3').onclick = override;
 document.getElementById('radio-4').onclick = override;
+
+// Change Auto-Button Colors if Active
+setInterval(function(){
+	if (document.getElementById('radio-1').checked) {
+		document.getElementsByClassName("auto-btn-1")[0].style.backgroundColor = "red"}
+	
+	else {
+		document.getElementsByClassName("auto-btn-1")[0].style.backgroundColor = null}
+	}
+	, 0);
+
+setInterval(function(){
+	if (document.getElementById('radio-2').checked) {
+		document.getElementsByClassName("auto-btn-2")[0].style.backgroundColor = "red"}
+	
+	else {
+		document.getElementsByClassName("auto-btn-2")[0].style.backgroundColor = null}
+	}
+	, 0);
+
+setInterval(function(){
+	if (document.getElementById('radio-3').checked) {
+		document.getElementsByClassName("auto-btn-3")[0].style.backgroundColor = "red"}
+	
+	else {
+		document.getElementsByClassName("auto-btn-3")[0].style.backgroundColor = null}
+	}
+	, 0);
+
+setInterval(function(){
+	if (document.getElementById('radio-4').checked) {
+		document.getElementsByClassName("auto-btn-4")[0].style.backgroundColor = "red"}
+	
+	else {
+		document.getElementsByClassName("auto-btn-4")[0].style.backgroundColor = null}
+	}
+	, 0);
 
 // Lines 3-11 of code pulled from: 
 // https://www.codingsnow.com/2021/01/image-slider-with-auto-play-manual.html
